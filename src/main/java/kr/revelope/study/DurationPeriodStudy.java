eb7051fbc6345fcc4345065bef3834ce3a0d1f0d
REVE <kgw1988@naver.com>
2019-10-13 19:37:48 +0900

    add answer

--- src/main/java/kr/revelope/study/DurationPeriodStudy.java
+++ src/main/java/kr/revelope/study/DurationPeriodStudy.java
@@ -1,6 +1,9 @@
 package kr.revelope.study;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.Period;
+import java.util.StringJoiner;
 
 public class DurationPeriodStudy {
 	/**
@@ -14,6 +17,40 @@ public class DurationPeriodStudy {
 	 *
 	 */
 	public String getDurationString(LocalDateTime startDateTime, LocalDateTime endDateTime) {
-		return null;
+		Period period = Period.between(startDateTime.toLocalDate(), endDateTime.toLocalDate());
+		int years = period.getYears();
+		int months = period.getMonths();
+		int days = period.getDays();
+
+		Duration duration = Duration.between(startDateTime, endDateTime);
+		int hours = (int)(duration.toHours() % 24);
+		int minutes = (int)(duration.toMinutes() % 60);
+		int seconds = (int)((duration.toMillis() / 1000) % 60);
+
+		return joinIfNotEmptyString(" ",
+			appendUnitIfMoreThanZero(years, "년"),
+			appendUnitIfMoreThanZero(months, "개월"),
+			appendUnitIfMoreThanZero(days, "일"),
+			appendUnitIfMoreThanZero(hours, "시간"),
+			appendUnitIfMoreThanZero(minutes, "분"),
+			appendUnitIfMoreThanZero(seconds, "초")
+		);
+	}
+
+	private String appendUnitIfMoreThanZero(int number, String unit) {
+		return number > 0 ? number + unit : "";
+	}
+
+	private String joinIfNotEmptyString(String delimiter, String... elements) {
+		StringJoiner joiner = new StringJoiner(delimiter);
+		for (String str : elements) {
+			if (str == null || str.equals("")) {
+				continue;
+			}
+
+			joiner.add(str);
+		}
+
+		return joiner.toString();
 	}
 }
--- src/main/java/kr/revelope/study/LocalDateTimeStudy.java
+++ src/main/java/kr/revelope/study/LocalDateTimeStudy.java
@@ -1,54 +1,66 @@
 package kr.revelope.study;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
 
 public class LocalDateTimeStudy {
 	/**
 	 * 들어온 날짜시간의 정각
 	 */
 	public LocalDateTime getJustAtTime(LocalDateTime localDateTime) {
-		return null;
+		return LocalDateTime.of(localDateTime.toLocalDate(), LocalTime.of(localDateTime.getHour(), 0, 0));
 	}
 
 	/**
 	 * 들어온 날짜시간의 정확히 한시간 뒤
 	 */
 	public LocalDateTime addHour(LocalDateTime localDateTime) {
-		return null;
+		return localDateTime.plusHours(1);
 	}
 
 	/**
 	 * 들어온 날짜시간의 1일
 	 */
 	public LocalDateTime getFirstDayOfMonth(LocalDateTime localDateTime) {
-		return null;
+		return localDateTime.withDayOfMonth(1);
 	}
 
 	/**
 	 * 들어온 날짜시간의 1일 자정
 	 */
 	public LocalDateTime getFirstDayMidnightOfMonth(LocalDateTime localDateTime) {
-		return null;
+		return LocalDateTime.of(localDateTime.toLocalDate().withDayOfMonth(1), LocalTime.MIDNIGHT);
 	}
 
 	/**
 	 * 들어온 날짜시간의 전월 마지막 날짜 정오
 	 */
 	public LocalDateTime getLastDayMidnightOfPreviousMonth(LocalDateTime localDateTime) {
-		return null;
+		return LocalDateTime.of(localDateTime.withDayOfMonth(1).minusDays(1).toLocalDate(), LocalTime.MIDNIGHT);
 	}
 
 	/**
 	 * 주어진 날짜시간이 30분보다 작으면 0분 0초, 아니면 30분 0초
 	 */
 	public LocalDateTime getOnTimeOrHalfTime(LocalDateTime localDateTime) {
-		return null;
+		LocalDate localDate = localDateTime.toLocalDate();
+		LocalTime localTime = localDateTime.toLocalTime();
+
+		int hour = localTime.getHour();
+		int minute = localTime.getMinute() < 30 ? 0 : 30;
+
+		return LocalDateTime.of(localDate, LocalTime.of(hour, minute, 0));
 	}
 
 	/**
 	 * 주어진 날짜시간이 한국시간(Asia/Seoul)이라고 할 때 영국시간(Europe/London)으로 변환
 	 */
 	public LocalDateTime changeSeoulTimeToLondon(LocalDateTime localDateTime) {
-		return null;
+		return localDateTime
+			.atZone(ZoneId.of("Asia/Seoul"))
+			.withZoneSameInstant(ZoneId.of("Europe/London"))
+			.toLocalDateTime();
 	}
 }
